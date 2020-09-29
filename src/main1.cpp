c887d785afeeee369cc6313f2e4afd6d14f838f7
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2020-09-29 15:21:06 +0300

    Add files via upload

---
+++ src/main1.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include "task1.h"
+
+using namespace std;
+
+
+
+int main() {
+    const size_t n = 6;
+    const char* arr[n]{ "f f f  f f", " f f f 2", "f ", "f  f", "v v v ", " f f fr r r " };
+    qsort(arr, n, sizeof(const char*), comp5);
+    const char* expected[n]{ "f ", "f  f", "v v v ", " f f f 2", "f f f  f f", " f f fr r r " };
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << ' ';
+    for (size_t i = 0; i < n; i++)
+        if (expected[i] != arr[i]) {
+            cout << "!";
+        }
+    return 0;
+}
