a7dcc8899eadf0924c8c43bbfb1efacb71556256
Ksuvot <ksuvot36@yandex.ru>
2020-09-29 16:54:30 +0300

    hot fix

--- src/task1.cpp
+++ src/task1.cpp
@@ -2,42 +2,52 @@
 
 int comp1( const void* a, const void* b )
 {
-    return ( *( int* )a - *( int* )b );
+    return ( *( int* )a - *( int* )b);
 }
 
 int comp2( const void* a, const void* b )
 {
-    return (*(double*)a - *(double*)b) > 0.0 ? 1 : -1;
+    double result = (*( double* )a - *( double* )b);
+    if (result > 0.0)
+        return 1;
+    else if (result < 0.0)
+        return -1;
+    else
+        return 0;
 }
 
 int comp3(const void *a, const void *b)
 {
-    return strcmp( *(char **) a, *(char **) b);
+    return strcmp(*(const char**)a, *(const char**)b);
 }
 
 int comp4(const void *a, const void *b)
 {
-    return strlen(*(char **) a) -  strlen(*(char **) b);
+    return strlen(*(const char**)a) - strlen(*(const char**)b);
 }
 
 int comp5( const void* a, const void* b )
 {
-    int num_a = 0;
-    int num_b = 0;
-    string str_a = *(const char **)a;
-    string str_b = *(const char **)b;
-    for (int i = 0; i < str_a.length(); i++){
-        if (str_a[i] == ' ')
-            num_a++;
+    const char* str1 = *(const char**)a;
+    const char* str2 = *(const char**)b;
+    int num1 = 0, num2 = 0, i = 0;
+
+    while (str1[i]){
+        if (str1[i] == ' ')
+            num1++;
+        i++;
     }
-    for (int i = 0; i < str_b.length(); i++){
-        if (str_b[i] ==  ' ')
-            num_b++;
+    i = 0;
+    while (str2[i]){
+        if (str2[i] == ' ')
+            num2++;
+        i++;
     }
-    return num_a - num_b;
+
+    return num1-num2;
 }
 
 int comp6( const void* a, const void* b )
 {
-    return (((Person*)a)->age - ((Person*)b)->age);
+    return (*(Person*)a).age - (*(Person*)b).age;
 }
