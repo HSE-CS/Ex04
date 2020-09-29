fb11e575663ad2f570cff8d28c604af69e0a24bb
Roma Balayan <roma1balayan@gmail.com>
2020-09-29 12:27:28 +0300

    Balayan 1st try

---
+++ include/task1.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+
+struct Person
+{
+    const char* name;
+    unsigned int age;
+};
+
+int comp1(const void* a, const void* b);
+
+int comp2(const void* a, const void* b);
+
+int comp3(const void* a, const void* b);
+
+int comp4(const void* a, const void* b);
+
+int comp5(const void* a, const void* b);
+
+int comp6(const void* a, const void* b);
