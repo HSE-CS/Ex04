63afe4930f8e78001598461a40df808994f14b3a
EgorBodrov <bodroff.alexeevich@yandex.ru>
2020-09-29 14:54:33 +0300

    b32

--- src/task1.cpp
+++ src/task1.cpp
@@ -12,26 +12,26 @@ int comp2(const void* a, const void* b)
 
 int comp3(const void* a, const void* b)
 {
-	return strcmp((char*)a, (char*)b);
+	return strcmp(*(char**)a, *(char**)b);
 }
 
 int comp4(const void* a, const void* b)
 {
-	return (strlen((char*)a) - strlen((char*)b));
+	return (strlen(*(char**)a) - strlen(*(char**)b));
 }
 
 int comp5(const void* a, const void* b)
 {
 	int c1 = 0;
 	int c2 = 0;
-	for (int i = 0; i < strlen((char*)a); i++)
+	for (int i = 0; i < strlen(*(char**)a); i++)
 	{
-		if (*((char*)a + i) == ' ')
+		if (*((char**)a)[i] == ' ')
 			c1++;
 	}
-	for (int i = 0; i < strlen((char*)b); i++)
+	for (int i = 0; i < strlen(*(char**)b); i++)
 	{
-		if (*((char*)b + i) == ' ')
+		if (*((char**)b)[i] == ' ')
 			c2++;
 	}
 	return c1 - c2;
