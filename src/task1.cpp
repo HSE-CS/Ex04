5fd81e96e4da3104f43d0a8764175c79dfac8808
Sergey Boryaev <somsom.som2@yandex.ru>
2020-09-29 12:11:28 +0300

    second try

--- src/task1.cpp
+++ src/task1.cpp
@@ -12,23 +12,23 @@ int comp2(const void * a, const void * b)
 
 int comp3(const void * a, const void * b)
 {
-    return strcmp((char*)a, (char*)b);
+    return strcmp(*((char**)a), *((char**)b));
 }
 
 int comp4(const void * a, const void * b)
 {
-    return (strlen((char*)a) - strlen((char*)b));
+    return (strlen(*((char**)a) - strlen(*((char**)b));
 }
 
 int comp5(const void * a, const void * b)
 {
     int count1 = 0;
     int count2 = 0;
-    for(unsigned int i=0; i<strlen((char*)a); i++)
-        if(((char*)a)[i]==' ')
+    for(unsigned int i=0; i<strlen(*((char**)a)); i++)
+        if(((*((char**)a))[i]==' ')
             count1++;
-    for(unsigned int i=0; i<strlen((char*)b); i++)
-        if(((char*)b)[i]==' ')
+    for(unsigned int i=0; i<strlen(*(char**)b); i++)
+        if((*((char**)b))[i]==' ')
             count2++;
     return count1 - count2;        
 }
