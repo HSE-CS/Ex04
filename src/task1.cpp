e399e63f73ddad57d34bc370a717dfdbf5ec3620
Stolbov Yaroslav <stojar2001@gmail.com>
2020-09-29 15:06:13 +0300

    all functions

--- src/task1.cpp
+++ src/task1.cpp
@@ -4,7 +4,7 @@
 
 #include <iostream>
 #include <cstring>
-
+#include "task1.h"
 int comp1(const void* a, const void* b){
     return ( *(int*)a - *(int*)b);
 }
@@ -19,13 +19,20 @@ int comp3(const void* a, const void* b){
 }
 
 int comp4(const void* a, const void* b){
-
+    return strlen(*(const char**)a) - strlen(*(const char**)b);
 }
 
 int comp5(const void* a, const void* b){
-
+    int count1 = 0, count2 = 0;
+    for (int i = 0; i < strlen(*(const char**)a); i++){
+        if (*(const char**)a == " ") count1++;
+    }
+    for (int i = 0; i < strlen(*(const char**)b); i++){
+        if (*(const char**)b == " ") count2++;
+    }
+    return count1-count2;
 }
 
-int comp6(const void* a, const void* b){
-
+int comp6(const void* a, const void* b) {
+    return ((*(Person *) a).age - (*(Person *) b).age);
 }
