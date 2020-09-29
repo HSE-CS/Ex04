094965e9dc73b7255aab7bc45c7307839554ca87
Safronov Ivan <50119705+SidNotSus@users.noreply.github.com>
2020-09-29 23:45:48 +0300

    testing

--- include/task1.h
+++ include/task1.h
@@ -6,14 +6,28 @@ struct Person
     unsigned age;
 };
 
-int comp1(const void* a, const void* b);
+int comp1(const void * a, const void * b){
+    return ( *( int* )a - *( int* )b );
+}
+int comp2(double *a, double *b){
+    //  return a - b;
+    if (*a-*b>0.0)
+        return 1;
+    else
+        return -1;
+}
+int comp3(const char* a, const char* b){
+    return strcmp(a, b);
+}
+int comp4(const char* a, const char* b){
+    return strlen(a) - strlen(b);
+}
+int comp5(const char* a, const char* b){
+    string str1(a);
+    string str2(b);
+    return count(str1.begin(), str1.end(), ' ') - count(str2.begin(), str2.end(), ' ') ;
+}
+int comp6(const void* a, const void* b) {
 
-int comp2(const void* a, const void* b);
-
-int comp3(const void* a, const void* b);
-
-int comp4(const void* a, const void* b);
-
-int comp5(const void* a, const void* b);
-
-int comp6(const void* a, const void* b);
+    return (((struct Person*)a)->age - ((struct Person*)b)->age);
+}
--- src/task1.cpp
+++ src/task1.cpp
@@ -5,31 +5,7 @@
 #include "../include/task1.h"
 using namespace std;
 
-int comp1(const void * a, const void * b){
-    return ( *( int* )a - *( int* )b );
-}
-int comp2(double *a, double *b){
-  //  return a - b;
-    if (*a-*b>0.0)
-        return 1;
-    else
-        return -1;
-}
-int comp3(const char* a, const char* b){
-    return strcmp(a, b);
-}
-int comp4(const char* a, const char* b){
-    return strlen(a) - strlen(b);
-}
-int comp5(const char* a, const char* b){
-    string str1(a);
-    string str2(b);
-    return count(str1.begin(), str1.end(), ' ') - count(str2.begin(), str2.end(), ' ') ;
-}
-int comp6(const void* a, const void* b) {
 
-    return (((struct Person*)a)->age - ((struct Person*)b)->age);
-}
 
 
 
