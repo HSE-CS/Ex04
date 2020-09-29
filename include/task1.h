c869cedf1620bb40d8ad2fa08c27ab4c00d484bd
Sergey Boryaev <somsom.som2@yandex.ru>
2020-09-29 11:55:40 +0300

    first try

---
+++ include/task1.h
@@ -0,0 +1,22 @@
+#ifndef TASK1_H
+#define TASK1_H
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+
+struct Person
+{
+    std::string name;
+    unsigned age;
+};
+
+int comp1(const void * a, const void * b);
+int comp2(const void * a, const void * b);
+int comp3(const void * a, const void * b);
+int comp4(const void * a, const void * b);
+int comp5(const void * a, const void * b);
+int comp6(const void * a, const void * b);
+
+#endif
