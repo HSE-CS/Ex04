557da951fa6c2ef4ceeab4eda33408d330a0d4b3
Stolbov Yaroslav <stojar2001@gmail.com>
2020-09-29 15:27:47 +0300

    all functions

--- src/task1.cpp
+++ src/task1.cpp
@@ -6,7 +6,7 @@
 #include <cstring>
 #include "task1.h"
 int comp1(const void* a, const void* b){
-    return ( *(int*)a - *(int*)b);
+    return (*(int *) a - *(int *) b);
 }
 
 int comp2(const void* a, const void* b) {
@@ -18,24 +18,24 @@ int comp2(const void* a, const void* b) {
 }
 
 int comp3(const void* a, const void* b){
-    return strcmp(*(const char**)a, *(const char**)b);
+    return (strcmp(*(char **) a, *(char **) b));
 }
 
 int comp4(const void* a, const void* b){
-    return (int)(strlen(*(const char**)a) - strlen(*(const char**)b));
+    return (int) (strlen(*(const char **) a) - strlen(*(const char **) b));
 }
 
 int comp5(const void* a, const void* b){
     int count1 = 0, count2 = 0;
     for (int i = 0; i < strlen(*(const char**)a); i++){
-        if ((*(const char**)a) == " ") count1++;
+        if ((*(const char **) a)[i] == ' ') count1++;
     }
     for (int i = 0; i < strlen(*(const char**)b); i++){
-        if ((*(const char**)b) == " ") count2++;
+        if ((*(const char**)b)[i] == ' ') count2++;
     }
     return count1-count2;
 }
 
 int comp6(const void* a, const void* b) {
-    return (int)((*(struct Person*) a).age - (*(struct Person*) b).age);
+    return (int)((*(Person*) a).age - (*(Person*) b).age);
 }
