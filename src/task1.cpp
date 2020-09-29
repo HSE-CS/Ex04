9dcfaac4d9f6b708b51aac5366d8c9bfe9a0e1d5
LukichevaPolina <lukichevapolina42@gmail.com>
2020-09-29 12:31:37 +0300

    Lukicheva Polina Ex04

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,5 +1,6 @@
 #include "task1.h"
-
+#include <stdio.h>
+#include <string.h>
 int comp1(const void* a, const void* b) {
 	return (*(int*)a - *(int*)b);
 }
@@ -9,18 +10,27 @@ int comp2(const void* a, const void* b) {
 }
 
 int comp3(const void* a, const void* b) {
-	return (*(const char*)a - *(const char*)b);
+	return strcmp(*(const char**)a, *(const char**)b);
 }
 
 int comp4(const void* a, const void* b) {
-	return (strlen((char*)a)- strlen((char*)b));
+	return (strlen(*(const char**)a)-strlen(*(const char**)b));
 }
 
 int comp5(const void* a, const void* b) {
-	return (*(const char*)a - *(const char*)b);
+	int count1 = 0, count2 = 0;
+	for (int i = 0; i<strlen(*(const char**)a); i++)
+		if ((*(const char**)a)[i] == ' ')
+			count1++;
+
+	for (int i = 0; i < strlen(*(const char**)b); i++)
+		if ((*(const char**)b)[i] == ' ')
+			count2++;
+		
+	return (count1 - count2);
 }
 
 int comp6(const void* a, const void* b) {
-	return (*(const char*)a - *(const char*)b);
+	return (((struct Person*) a)->age - ((struct Person*)b)->age);
 }
 
