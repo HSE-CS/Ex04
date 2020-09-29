c4ea3623311b6cd51cfcb777bde052b89f33343e
sharg <jenya.sharunov@gmail.com>
2020-09-29 14:53:07 +0300

    Sharunov EvgenyV2 14.53

--- include/task1.h
+++ include/task1.h
@@ -6,9 +6,9 @@ struct Person
     std::string  name;
     unsigned age;
 };
-bool comp1(int first,int second);
-bool comp2(double first,double second);
-bool comp3(const char *first,const char *second);
-bool comp4(const char *first,const char *second);
-bool comp5(const char *first,const char *second);
-bool comp6(Person *first,Person *second);
+int comp1(int first,int second);
+int comp2(double first,double second);
+int comp3(const char *first,const char *second);
+int comp4(const char *first,const char *second);
+int comp5(const char *first,const char *second);
+int comp6(Person *first,Person *second);
--- src/task1.cpp
+++ src/task1.cpp
@@ -5,19 +5,26 @@
 
 #include <cstring>
 
-bool comp1(int first,int second){
-    return first<second;
+int comp1(int first,int second){
+    return first-second;
 }
-bool comp2(double first,double second){
-    return first<second;
+int comp2(double first,double second){
+    if ((first - second) < 0)
+        return  -1;
+
+    else if ((first - second) > 0)
+        return 1;
+
+    else
+        return 0;
 }
-bool comp3(const char *first,const char *second){
-    return first<second;
+int comp3(const char *first,const char *second){
+    return strcmp(first,second);
 }
-bool comp4(const char *first,const char *second){
-    return strlen(first)<strlen(second);
+int comp4(const char *first,const char *second){
+    return strlen(first)-strlen(second);
 }
-bool comp5(const char *first,const char *second){
+int comp5(const char *first,const char *second){
     int numOfSpacesInFirst = 0, numOfSpacesInSecond = 0;
     for (int i = 0; i < strlen(first); ++i) {
         if (first[i] == ' '){
@@ -29,8 +36,8 @@ bool comp5(const char *first,const char *second){
             numOfSpacesInSecond ++;
         }
     }
-    return numOfSpacesInFirst<numOfSpacesInSecond;
+    return numOfSpacesInFirst-numOfSpacesInSecond;
 }
-bool comp6(Person* first,Person* second){
-    return  first->age<second->age;
+int comp6(Person* first,Person* second){
+    return  (*first).age-(*second).age;
 }
