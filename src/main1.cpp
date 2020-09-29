86140952193157994a96bc1844f585fd81e78665
sharg <jenya.sharunov@gmail.com>
2020-09-29 13:38:47 +0300

    Sharunov EvgenyV2 Second Commit

---
+++ src/main1.cpp
@@ -0,0 +1,7 @@
+//
+// Created by sharg on 29.09.2020.
+//
+
+int main(){
+    return 0;
+}
---
+++ src/task1.cpp
@@ -0,0 +1,33 @@
+//
+// Created by sharg on 29.09.2020.
+//
+#include "task1.h"
+bool comp1(int first,int second){
+    return first<second;
+}
+bool comp2(double first,double second){
+    return first<second;
+}
+bool comp3(const char *first,const char *second){
+    return first<second;
+}
+bool comp4(const char *first,const char *second){
+    return strlen(first)<strlen(second);
+}
+bool comp5(const char *first,const char *second){
+    int numOfSpacesInFirst = 0, numOfSpacesInSecond = 0;
+    for (int i = 0; i < strlen(first); ++i) {
+        if (first[i] == ' '){
+            numOfSpacesInFirst ++;
+        }
+    }
+    for (int i = 0; i < strlen(second); ++i) {
+        if (second[i] == ' '){
+            numOfSpacesInSecond ++;
+        }
+    }
+    return numOfSpacesInFirst<numOfSpacesInSecond;
+}
+bool comp6(Person* first,Person* second){
+    return  first->age<second->age;
+}
