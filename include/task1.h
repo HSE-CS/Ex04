9d1d31bb43ba7ea3555afb0204542865aaa40a0a
Stolbov Yaroslav <stojar2001@gmail.com>
2020-09-29 14:41:37 +0300

    test 3 functions

--- include/task1.h
+++ include/task1.h
@@ -11,4 +11,9 @@ struct Person
 };
 
 int comp1(const void* a, const void* b);
+int comp2(const void* a, const void* b);
+int comp3(const void* a, const void* b);
+int comp4(const void* a, const void* b);
+int comp5(const void* a, const void* b);
+int comp6(const void* a, const void* b);
 #endif //TASK1_TASK1_H
--- src/task1.cpp
+++ src/task1.cpp
@@ -3,7 +3,29 @@
 //
 
 #include <iostream>
+#include <cstring>
 
 int comp1(const void* a, const void* b){
     return ( *(int*)a - *(int*)b);
+}
+
+int comp2(const void* a, const void* b) {
+    if (*(double*) a >= *(double*) b) return 1;
+    return -1;
+}
+
+int comp3(const void* a, const void* b){
+    return strcmp(*(const char**)a, *(const char**)b);
+}
+
+int comp4(const void* a, const void* b){
+
+}
+
+int comp5(const void* a, const void* b){
+
+}
+
+int comp6(const void* a, const void* b){
+
 }
