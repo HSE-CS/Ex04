5c84f8285fd6e16f87347e9865a474a7efb91b10
VarginDimitry <dimitry.vargin@yandex.ru>
2020-09-30 22:56:05 +0300

    V.3

--- src/task1.cpp
+++ src/task1.cpp
@@ -8,19 +8,22 @@ int comp1( const void* a, const void* b ) {
 }
 
 int comp2( const void* a, const void* b ) {
-    return (int)( *( double* )a - *( double* )b );
+    double temp = ( *( double* )a - *( double* )b );
+    if (temp > 0.0)
+        return 1;
+    return 0;
 }
 
 int comp3( const void* a, const void* b ){
-    return strcmp((char*)a, (char*)b);
+    return strcmp(*(char**)a, *(char**)b);
 }
 
 int comp4( const void* a, const void* b ){
-    return (strlen((char*)a) - strlen((char*)b));
+    return (strlen(*(char**)a) - strlen(*(char**)b));
 }
 
 int comp5( const void* a, const void* b ){
-    return (charCounter((char*)a, ' ') - charCounter((char*)b, ' '));
+    return (charCounter(*(char**)a, ' ') - charCounter(*(char**)b, ' '));
 }
 
 int comp6( const void* a, const void* b ){
