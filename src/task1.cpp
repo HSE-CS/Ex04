aca9917089f3d6722b067ff219b802bcb47b0c88
Vladislav-Kanev <71171071+Vladislav-Kanev@users.noreply.github.com>
2020-10-05 16:47:16 +0300

    fifth attempt

--- src/task1.cpp
+++ src/task1.cpp
@@ -8,25 +8,27 @@ int comp1(const void* a, const void* b) {
     return (*(int*)a - *(int*)b);
 }
 int comp2(const void* a, const void* b) {
-    return int((*(double*)b - *(double*)a));
+    if (*(long double*)a > * (long double*)b)
+        return 1;
+    else if (*(long double*)a < *(long double*)b) return -1;
+    else return 0;
 }
 int comp3(const void* a, const void* b) {
-    return -strcmp((const char*)a, (const char*)b);
+    return strcmp(*((char**)a), *((char**)b));;
 }
 int comp4(const void* a, const void* b) {
-    return -(strlen((const char*)a) - strlen((const char*)b));
+    return (strlen(*((char**)a)) - strlen(*((char**)b)));
 }
 int comp5(const void* a, const void* b) {
-    int count1 = 0, count2 = 0;
-    const char* s1 = (const char*)a;
-    const char* s2 = (const char*)b;
-    for (int i = 0; i < strlen((const char*)a); i++) {
-        if (s1[i] == ' ') count1++;
-    }
-    for (int i = 0; i < strlen((const char*)b); i++) {
-        if (s2[i] == ' ') count2++;
-    }
-    return -count1 + count2;
+    int count1 = 0;
+    int count2 = 0;
+    for (unsigned int i = 0; i < strlen(*((char**)a)); i++)
+        if ((*((char**)a))[i] == ' ')
+            count1++;
+    for (unsigned int i = 0; i < strlen(*(char**)b); i++)
+        if ((*((char**)b))[i] == ' ')
+            count2++;
+    return count1 - count2;
 }
 int comp6(const void* a, const void* b) {
     Person* p1 = (Person*)a;
