06c8f3c1235483bfe1d32a2eff9fd490f59377be
TimurZaytsev <tz200128@gmail.com>
2020-10-05 15:57:42 +0300

    second

--- src/task1.cpp
+++ src/task1.cpp
@@ -19,7 +19,7 @@ int comp3(const void* a, const void* b) {
 };
 
 int comp4(const void* a, const void* b) {
-	return (strlen(*((char*)a)) - strlen(*((char*)b)));
+	return (strlen(*((char**)a)) - strlen(*((char**)b)));
 };
 
 int comp5(const void* a, const void* b) {
