882c84c3a39f42948f114a704f291a9ccfda6ee8
KuznetsovMikhail <KuznetsovMikhail>
2020-10-05 15:25:12 +0300

    Кузнецов Михаил

---
+++ include/task1.h
@@ -0,0 +1,22 @@
+#include <string>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+#ifndef TASK1_H_INCLUDED
+#define TASK1_H_INCLUDED
+int comp1 (const void *a, const void *b);
+int comp2 (const void *a, const void *b);
+int comp3 (const void *a, const void *b);
+int comp4 (const void *a, const void *b);
+int comp5 (const void *a, const void *b);
+int comp6 (const void *a, const void *b);
+struct Person
+{
+    string   name;
+    unsigned age;
+}
+
+
+#endif // TASK1_H_INCLUDED
