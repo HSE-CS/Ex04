e2704c625bc69358248d62fd0d638d2eadfcd33e
valvarl <valvarl@ya.ru>
2020-10-05 15:16:40 +0300

    task1.h, task1.cpp

--- include/task1.h
+++ include/task1.h
@@ -2,7 +2,25 @@
 // Created by freeb on 05.10.2020.
 //
 
+#include <string>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
 #ifndef TASK1_TASK1_H
 #define TASK1_TASK1_H
+
+struct Person
+{
+    string name;
+    unsigned age;
+};
+
+int comp1(const void* a, const void* b);
+int comp2(const void* a, const void* b);
+int comp3(const void* a, const void* b);
+int comp4(const void* a, const void* b);
+int comp5(const void* a, const void* b);
+int comp6(const void* a, const void* b);
 
 #endif //TASK1_TASK1_H
--- src/task1.cpp
+++ src/task1.cpp
@@ -2,3 +2,29 @@
 // Created by freeb on 05.10.2020.
 //
 
+#include "task1.h"
+
+int comp1( const void* a, const void* b ) {
+    return ( *( int* )a - *( int* )b );
+}
+
+int comp2( const void* a, const void* b ) {
+    return (int)( *( double * )a - *( double * )b );
+}
+
+int comp3( const void* a, const void* b ) {
+    return strcmp((char *)a, (char *)b);
+}
+
+int comp4(const void *a, const void *b) {
+    return (int)(strlen((char *)a) - strlen((char *)b));
+}
+
+int comp5(const void *a, const void *b) {
+    return (int)(count((char *)a, (char *)a + strlen((char *)a), ' ') -
+    count((char *)b, (char *)b + strlen((char *)b), ' '));
+}
+
+int comp6(const void *a, const void *b) {
+    return (int)(*(Person *)a).age - (int)(*(Person *)b).age;
+}
