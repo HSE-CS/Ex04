f1170f795b1f1b299c5b4de5cf1a2ba63ce58e11
Vladislav-Kanev <71171071+Vladislav-Kanev@users.noreply.github.com>
2020-10-05 16:00:10 +0300

    third attempt

--- src/task1.cpp
+++ src/task1.cpp
@@ -8,13 +8,13 @@ int comp1(const void* a, const void* b) {
     return (*(int*)a - *(int*)b);
 }
 int comp2(const void* a, const void* b) {
-    return int((*(double*)a - *(double*)b));
+    return int((*(double*)b - *(double*)a));
 }
 int comp3(const void* a, const void* b) {
-    return strcmp((const char*)a, (const char*)b);
+    return -strcmp((const char*)a, (const char*)b);
 }
 int comp4(const void* a, const void* b) {
-    return (strlen((const char*)a) - strlen((const char*)b));
+    return -(strlen((const char*)a) - strlen((const char*)b));
 }
 int comp5(const void* a, const void* b) {
     int count1 = 0, count2 = 0;
@@ -26,7 +26,7 @@ int comp5(const void* a, const void* b) {
     for (int i = 0; i < strlen((const char*)b); i++) {
         if (s2[i] == ' ') count2++;
     }
-    return count1 - count2;
+    return -count1 + count2;
 }
 int comp6(const void* a, const void* b) {
     Person* p1 = (Person*)a;
