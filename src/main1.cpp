cef3b7d89ea962e8fbcbdca94d6973950bd02689
ch2oh-ch2oh <valerasofronov01@gmail.com>
2020-10-05 15:30:48 +0300

    :3

---
+++ src/main1.cpp
@@ -0,0 +1,9 @@
+#include "task1.h"
+
+using namespace std;
+
+int main()
+{
+	return 0;
+} 
+
---
+++ src/task1.cpp
@@ -0,0 +1,46 @@
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include "task1.h"
+
+int comp1(const void* a, const void* b){
+    return (*(int*)a-*(int*)b) == 0?0:((*(int*)a-*(int*)b)>0?1:-1)
+}
+
+int comp2(const void* a, const void* b){
+    if (*(double*)a - *(double*)b > 0.0)
+		return 1;
+	else if (*(double*)a - *(double*)b < 0.0)
+		return -1;
+	else if (*(double*)a - *(double*)b < 0.000001)||(*(double*)a - *(double*)b > -0.000001)
+		return 0;
+}
+
+int comp3(const void* a, const void* b){
+    return (strcmp(*((char**)a), *((char**)b)));
+}
+
+int comp4(const void* a, const void* b){
+    int a=strlen(*((char**)a)) - strlen(*((char**)b));
+    return (a==0?0:(a>0?1:-1));
+}
+int comp5(const void* a, const void* b){
+    unsigned int n = 0, m = 0;
+	for (unsigned int i = 0; i < strlen(*((char**)a)); i++)
+	{
+		if ((*((char**)a))[i] == ' ')
+			n++;
+	}
+	for (unsigned int i = 0; i < strlen(*((char**)b)); i++)
+	{
+		if ((*((char**)b))[i] == ' ')
+			m++;
+	}
+    int a=n-m;
+	return (a==0?0:(a>0?1:-1));
+    }
+
+int comp6(const void* a, const void* b){
+    int a=((Person*)a)->age - ((Person*)b)->age;
+    return (a==0?0:(a>0?1:-1));
+}
