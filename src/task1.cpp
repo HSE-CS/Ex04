f5f8890a4289e6a5a9df4b2d1fee78d47d24b9b6
EgorBuzanov <egor4ek007@gmail.com>
2020-10-05 15:18:06 +0300

    Egor Buzanov

--- src/task1.cpp
+++ src/task1.cpp
@@ -17,22 +17,22 @@ int comp2(const void *a, const void *b)
 
 int comp3(const void *a, const void *b)
 {
-    return strcmp((const char *)a, (const char *)b);
+    return strcmp(*(char **)a, *(char **)b);
 };
 
 int comp4(const void *a, const void *b)
 {
-    return (strlen((const char *)a) - strlen((const char *)b));
+    return (strlen(*(char **)a) - strlen(*(char **)b));
 };
 
 int comp5(const void *a, const void *b)
 {
     unsigned int c = 0, d = 0, len = 0;
-    len = strlen((const char *)a);
+    len = strlen(*(char **)a);
     for (int i = 0; i < len; ++i)
         if ((*((char **)a))[i] == ' ')
             ++c;
-    len = strlen((const char *)b);
+    len = strlen(*(char **)b);
     for (int i = 0; i < len; ++i)
         if ((*((char **)b))[i] == ' ')
             ++d;
