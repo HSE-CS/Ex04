ac39242cfa7d1d9087664f910c099e8688d8aba8
k-dash6 <71211839+k-dash6@users.noreply.github.com>
2020-10-05 23:02:33 +0300

    n1

---
+++ src/task1.cpp
@@ -0,0 +1,11 @@
+#include "task1.h"
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <string.h>
+
+int comp1(const void* a, const void* b) {
+	return (*(int*)a - *(int*)b);
+}
+
+
