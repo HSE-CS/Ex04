06572bab73e79bc89e45f3feb3c7b80397014933
HabKaffee <neknaum1@gmail.com>
2020-10-05 17:33:56 +0300

    check of comp4

--- src/task1.cpp
+++ src/task1.cpp
@@ -29,7 +29,7 @@ int comp3(const void* a, const void* b) {
 
 
 int comp4(const void* a, const void* b) {
-	return (strlen((*(char**)a) - strlen((*(char**)b))));
+	return (strlen(*((char**)a)) - strlen(*((char**)b)));
 }
 
 
