d374e6a0ea75bda4fce05c375249ba9b08aaf9c4
Anton Klykov <antonklykov01@mail.ru>
2020-10-05 14:00:00 +0300

    Делаю 1 отправку

---
+++ include/task1.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+struct Person {
+	std::string name;
+	unsigned int age;
+};
+
+int comp1(void* a, void* b);
+int comp2(void* a, void* b);
+int comp3(void* a, void* b);
+int comp4(void* a, void* b);
+int comp5(void* a, void* b);
+int comp6(void* a, void* b);
---
+++ src/task1.cpp
@@ -0,0 +1,40 @@
+#include "task1.h"
+#include <cstring>
+#include <iosream>
+
+int comp1(void* a, void* b) {
+	return ( *((int*)a - *((int*)b);
+}
+
+int comp2(void* a, void* b) {
+	return ( *((double*)a - *((double*)b);
+}
+
+int comp3(void* a, void* b) {
+	return strcmp((const char*)a, (const char*)b);
+}
+
+int comp4(void* a, void* b) {
+	return strlen((const char*)a) - strlen((const char*)b);
+}
+
+int comp5(void* a, void* b) {
+	int count1 = 0;
+	int count2 = 0;
+	for(int i = 0; ((const char*)a)[i] != '\0'; i++) {
+		if ((const char*)a)[i] == ' ') {
+			count1++;	
+		}
+	}
+	for(int i = 0; ((const char*)b)[i] != '\0'; i++) {
+		if ((const char*)b)[i] == ' ') {
+			count2++;
+		}
+	}
+	return count1 - count2;
+}
+
+int comp6(void* a, void* b) {
+	return (*(Person*)a)->age - (*(Person*)b)->age;
+}
+
