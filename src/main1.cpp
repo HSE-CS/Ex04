7835db9eac141e9a010dc8167f14efdb34c54229
Alexander Grachev <s89519011368@yandex.ru>
2020-10-05 16:27:32 +0300

    changes 1.1.1

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,9 +1,26 @@
+#include <iostream>
 #include "task1.h"
+using namespace std;
 
-int main(){
+int main()
+{
+//const int n = 6;
+/*    const size_t n = 3;
+    Person arr[n]{ {"a",34},{"b",48},{"c",22} };
+    qsort(arr, n, sizeof(Person), comp6);
+    Person expected[n]{ {"c",22},{"a",34},{"b",48} };
+
+    for (auto i : arr)
+        cout << i.age << ' '<< i.name << endl;
 
 
+    const size_t n = 6;
+    const char* arr[n]{"nnn", "uuu", "kkk", "ppp", "sss", "ttt"};
+    qsort(arr,n,sizeof(const char*),comp3);
+    const char* expected[n]{"kkk", "nnn", "ppp", "sss", "ttt", "uuu"};
+
+    for (auto i : arr)
+        cout << i << endl;
+*/
     return 0;
-
 }
-
--- src/task1.cpp
+++ src/task1.cpp
@@ -21,22 +21,22 @@ int comp2(const void* a, const void* b) {
 }
 
 int comp3(const void* a, const void* b) {
-    for (int i = 0; ((char*)a + i) == nullptr || ((char*)b+ i) == nullptr; i++) {
-        if (*((char*)a + i) == *((char*)b + i)) {
+    for (int i = 0; (**(char**)a + i) != NULL && (**(char**)b+ i) != NULL; i++) {
+        if (**((char**)a + i) == **((char**)b + i)) {
             continue;
         }
-        return *(char*)a - *(char*)b;
+        return **(char**)a - **(char**)b;
     }
     return 0;
 }
 
 int comp4(const void* a, const void* b) {
-    return strlen((char*)a) - strlen((char*)b);
+    return strlen(*(char**)a) - strlen(*(char**)b);
 }
 
 int comp5(const void* a, const void* b) {
-    std::string str1((char*)a);
-    std::string str2((char*)b);
+    std::string str1(*(char**)a);
+    std::string str2(*(char**)b);
     return std::count(str1.begin(), str2.end(), ' ') - count(str2.begin(), str2.end(), ' ');
 }
 
