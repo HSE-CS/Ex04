ef901ae9b69a1f540dc68c31365596060c2a7c65
Vladislav-Kanev <71171071+Vladislav-Kanev@users.noreply.github.com>
2020-10-05 16:59:31 +0300

    seventh attempt

--- src/task1.cpp
+++ src/task1.cpp
@@ -8,10 +8,7 @@ int comp1(const void* a, const void* b) {
     return (*(int*)a - *(int*)b);
 }
 int comp2(const void* a, const void* b) {
-    if (*(double*)a > * (double*)b)
-        return -1;
-    else if (*(double*)a < *(double*)b) return 1;
-    else return 0;
+    return (*(double*)a - *(double*)b) > 0.0 ? 1 : -1;
 }
 int comp3(const void* a, const void* b) {
     return strcmp(*((char**)a), *((char**)b));;
