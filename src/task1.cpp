d8bc906c6b385311fd20805da2f9d1066089a345
Anton Klykov <antonklykov01@mail.ru>
2020-10-05 14:24:36 +0300

    Делаю 1 отправку, с 4 корректировкой

--- src/task1.cpp
+++ src/task1.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int comp1(void* a, void* b) {
-	return ( *((int*)a - *((int*)b) );
+	return (*((int*)a) - *((int*)b));
 }
 
 int comp2(void* a, void* b) {
@@ -35,6 +35,6 @@ int comp5(void* a, void* b) {
 }
 
 int comp6(void* a, void* b) {
-	return ((*(Person*)a)->age - (*(Person*)b)->age);
+	return ((Person*)a->age - (Person*)b->age);
 }
 
