046632bd2687274bf2d007a8e758039c0462c376
Мария <71264966+Karcevamd@users.noreply.github.com>
2020-10-05 21:19:07 +0300

    karcevamd

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,14 +1,8 @@
-#include <algorithm>
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
 #include "task1.h"
-#include <fstream>
-#include <string.h>
 
 using namespace std;
 
-int main() {
-   
-    return 0;
+int main()
+{
+	return 0;
 }
--- src/task1.cpp
+++ src/task1.cpp
@@ -1,40 +1,39 @@
 #include "task1.h"
-#include <string.h>
-#include <string>
-#include<algorithm>
-#include<math.h>
-
-using namespace std;
 
 int comp1(const void* a, const void* b) {
-    return (*(int*)a - *(int*)b);
+	return (*(int*)a - *(int*)b) == 0 ? 0 : ((*(int*)a - *(int*)b) > 0 ? 1 : -1);
 }
-int comp2(const void *a, const void *b) {
-    if (*(double *) a > *(double *) b) {
-        return 1;
-    }
-    if (*(double *) a < *(double *) b) {
-        return -1;
-    }
-    if (*(double *) a == *(double *) b) {
-        return 0;
-    }
+int comp2(const void* a, const void* b) {
+	if (*(double*)a - *(double*)b > 0.0)
+		return 1;
+	else if (*(double*)a - *(double*)b < 0.0)
+		return -1;
+	else if ((*(double*)a - *(double*)b < 0.000001) || (*(double*)a - *(double*)b > -0.000001))
+		return 0;
 }
 int comp3(const void* a, const void* b) {
-    return strcmp(*((char**)a), *((char**)b));;
+	return (strcmp(*((char**)a), *((char**)b)));
 }
 int comp4(const void* a, const void* b) {
-    return (strlen(*((char**)a)) - strlen(*((char**)b)));
+	int i = strlen(*((char**)a)) - strlen(*((char**)b));
+	return (i == 0 ? 0 : (i > 0 ? 1 : -1));
 }
-int comp5(const void * a, const void * b){
-    std::string s1 = *(char**)a;
-    std::string s2 = *(char**)b;
-    int space_amount1 = std::count(s1.begin(),s1.end(),' ');
-    int space_amount2 = std::count(s2.begin(),s2.end(),' ');
-    return space_amount1 - space_amount2;
+int comp5(const void* a, const void* b) {
+	unsigned int n = 0, m = 0;
+	for (unsigned int i = 0; i < strlen(*((char**)a)); i++)
+	{
+		if ((*((char**)a))[i] == ' ')
+			n++;
+	}
+	for (unsigned int i = 0; i < strlen(*((char**)b)); i++)
+	{
+		if ((*((char**)b))[i] == ' ')
+			m++;
+	}
+	int i = n - m;
+	return (i == 0 ? 0 : (i > 0 ? 1 : -1));
 }
 int comp6(const void* a, const void* b) {
-    Person* p1 = (Person*)a;
-    Person* p2 = (Person*)b;
-    return p1->age - p2->age;
+	int i = ((Person*)a)->age - ((Person*)b)->age;
+	return (i == 0 ? 0 : (i > 0 ? 1 : -1));
 }
