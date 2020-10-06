2f73b8056ec03fdfe0c487e7a6185d1870fcf031
Niki <you@example.com>
2020-10-06 16:13:46 +0300

    well

--- src/task1.cpp
+++ src/task1.cpp
@@ -17,23 +17,23 @@ int comp2(const void* a, const void* b) {
 }
 
 int comp3(const void* s1, const void* s2) {
-	return (strcmp((char*)s1, (char*)s2));
+	return (strcmp(*(char**)s1, *((char**)s2)));
 }
 
 int comp4(const void* s1, const void* s2) {
-	return (strlen((char*)s1) - strlen((char*)s2));
+	return (strlen(*((char**)s1)) - strlen(*((char**)s2)));
 }
 
 int comp5(const void* s1, const void* s2) {
 	unsigned int len1 = 0, len2 = 0;
-	for (unsigned int i = 0; i < strlen((char*)s1); i++)
+	for (unsigned int i = 0; i < strlen(*((char**)s1)); i++)
 	{
-		if (((char*)s1)[i] == ' ')
+		if ((*((char**)s1))[i] == ' ')
 			len1++;
 	}
-	for (unsigned int i = 0; i < strlen((char*)s2); i++)
+	for (unsigned int i = 0; i < strlen(*((char**)s2)); i++)
 	{
-		if (((char*)s2)[i] == ' ')
+		if ((*((char**)s2))[i] == ' ')
 			len2++;
 	}
 	return (len1 - len2);
