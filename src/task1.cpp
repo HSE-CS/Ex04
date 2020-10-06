f96bdc4d25aeed75af0ba4eb0a674a0d484d8a3f
Olesya Nikolaeva <63335416+olesyani@users.noreply.github.com>
2020-10-06 11:52:07 +0300

    Update task1.cpp

--- src/task1.cpp
+++ src/task1.cpp
@@ -2,46 +2,61 @@
 
 using namespace std;
 
-int comp1( const void * a, const void * b ) {
-    return ( *( int* )a - *( int* )b );
+int comp1(const void* a, const void* b) {
+
+    return (*(int*)a - *(int*)b);
+
 }
 
-int comp2( const void * a, const void * b ) {
-    double c = ( *( double* )a - *( double* )b );
+
+int comp2(const void* a, const void* b) {
+
+    double c = (*(double*)a - *(double*)b);
     if (c > 0)
         return 1;
-    else
+    else 
         if (c < 0)
             return -1;
     else
         return 0;
+
 }
 
-int comp3( const void * a, const void * b ) {
-    return (strcmp(*((char**)a), *((char**)b)));
+int comp3(const void* a, const void* b) {
+
+    return strcmp(*(const char**)a, *(const char**)b);
+
 }
 
-int comp4( const void * a, const void * b ) {
-    return (strlen(*((char**)a)) - strlen(*((char**)b)));
+int comp4(const void* a, const void* b) {
+
+    return (strlen(*(const char**)a) - strlen(*(const char**)b));
+
 }
 
-int comp5( const void * a, const void * b ) {
-    std::string str1 = *(const char**)a;
-    std::string str2 = *(const char**)b;
-    int c = 0;
-    int d = 0;
-    for (char ch1 : str1) {
-        if (ch1 == ' ')
-            c += 1;
+int comp5(const void* a, const void* b) {
+
+    int count1 = 0;
+    int count2 = 0;
+    std::string s1 = *(const char**)a;
+    std::string s2 = *(const char**)b;
+
+    for (char i : s1) {
+        if (i == ' ')
+            count1 = count1 + 1;
     }
-    for (char ch2 : str2) {
-        if (ch2 == ' ')
-            d += 1;
+
+    for (char j : s2) {
+        if (j == ' ')
+            count2 = count2 + 1;
     }
-    return ( c - d );
+
+    return (count1 - count2);
+
 }
 
-int comp6( const void * a, const void * b ) {
-    return (( *( Person* )a).age - (*( Person* )b).age );
-}
+int comp6(const void* a, const void* b) {
 
+    return ((*(Person*)a).age - (*(Person*)b).age);
+
+}
