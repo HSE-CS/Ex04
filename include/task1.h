165cdacfe5496e0a7dcbe97ffe00b81153e599d2
NAKAUTT <khorev.egor@list.ru>
2020-10-15 15:10:33 +0300

    Egor Khorev 19PMI2 Ex04

---
+++ include/task1.h
@@ -0,0 +1,20 @@
+#ifndef task1_h
+#define task1_h
+#include <stdio.h>
+#include <cstring>
+#include <string>
+using namespace std;
+
+struct Person {
+	string name;
+	unsigned age;
+};
+
+int comp1(const void * a, const void * b);
+int comp2(const void * a, const void * b);
+int comp3(const void * a, const void * b);
+int comp4(const void * a, const void * b);
+int comp5(const void * a, const void * b);
+int comp6(const void * a, const void * b);
+
+#endif 
