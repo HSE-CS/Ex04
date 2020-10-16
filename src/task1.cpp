87a078c037adf7c9013fc84c366a7ca9eab33d98
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2020-10-16 18:39:50 +0300

    Update task1.cpp

--- src/task1.cpp
+++ src/task1.cpp
@@ -42,24 +42,23 @@ int comp4(const void* a, const void* b) {
 
 int comp5(const void* a, const void* b) {
 
-	int n = 0;
-	int m = 0;
-
-	for (int i = 0; i < strlen(*(char**)a); i++) {
-
-		if ((*(char**)a)[i] = ' ')
-			n += 1;
-
+	char* str1 = (*(char**)a);
+	char* str2 = (*(char**)b);
+	int l1 = strlen(str1);
+	int l2 = strlen(str2);
+	int a1 = 0;
+	int a2 = 0;
+	for (int i = 0; i < l1; i++)
+	{
+		if (str1[i] == ' ')
+			a1++;
 	}
-
-	for (int i = 0; i < strlen(*(char**)b); i++) {
-
-		if ((*(char**)b)[i] = ' ')
-			m += 1;
-
+	for (int i = 0; i < l2; i++)
+	{
+		if (str2[i] == ' ')
+			a2++;
 	}
-
-	return (n - m);
+	return (a1 - a2);
 
 }
 
