d1742b879760cadfb9d79e79dbe9de7a7e34fdff
RBurtsev <r.a.burtsev@mail.ru>
2020-10-16 21:48:38 +0300

    'com2

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,6 @@
 
 using namespace std;
 
-struct Person
-{
-	string   name;
-	unsigned age;
-};
-
 int main() {
-	
-	const size_t n = 6;
-	const char* arr[n]{ "nnn", "uuu", "kkk", "ppp", "sss", "ttt" };
-	qsort(arr, n, sizeof(const char*), comp3);
-	for (int i = 0; i < n; i++) {
-		cout << arr[i] << ' ';
-	}
-	system("Pause");
 	return 0;
 }
