e2633a26bf8e9cc101952eb2a18cfe4614715373
Your NameVadukk <Vadik-Tumakov.com@yandex.ru>
2020-11-11 10:43:17 +0300

    Tumakov Vadim

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,11 +1,9 @@
-return (*(int*)a - *(int*)b);
-}
-int comp2(const void* a, const void* b) {
-	if (*(double*)a > * (double*)b)
-		return -1;
-	else if (*(double*)a < *(double*)b) return 1;
-	else return 0;
-	return (*(double*)a - *(double*)b) > 0.0 ? 1 : -1;
-}
 int comp3(const void* a, const void* b) {
-	return strcmp(*((char**)a), *((char**)b));;
+};
+
+int comp4(const void* a, const void* b) {
+	return (strlen(*((char*)a)) - strlen(*((char*)b)));
+	return (strlen(*((char**)a)) - strlen(*((char**)b)));
+};
+
+int comp5(const void* a, const void* b) {
